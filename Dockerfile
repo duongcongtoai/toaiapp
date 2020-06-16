8a2f173a26ec0a21e4d5082577daba766ee6e93c
Duong Cong Toai <duongcongtoai.fjc@gmail.com>
2020-06-16 07:40:55 +0700

    fix postgres service not ready when app connect for first deployment

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ ENV APP_ENV $app_env
 COPY . /toaiapp
 WORKDIR /toaiapp 
 
+RUN apt-get update \
+    && apt-get install -y postgresql-client 
+
 RUN go get ./app
 # RUN go build
 RUN go get github.com/cespare/reflex
---
+++ wait-for-postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+until PGPASSWORD=toai psql -h "$host" -U "toai" "toai_app" -W -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
